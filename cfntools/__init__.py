827b67de21e7121d13aabc16fa6e403fa90ec70a
Borislav Ivanov <borogl@gmail.com>
2017-02-09 23:30:47 -0800

    cookiecutter gh:kragniz/cookiecutter-pypackage-minimal

---
+++ cfntools/__init__.py
@@ -0,0 +1,5 @@
+"""cfn-tools - tralala"""
+
+__version__ = '0.1.0'
+__author__ = 'Borislav Ivanov <borogl@gmail.com>'
+__all__ = []
---
+++ setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+requirements = [
+    'Click>=6.0',
+    'boto3>=1.3.1'
+]
+
+setuptools.setup(
+    name="cfn-tools",
+    version="0.1.0",
+    url="https://github.com/boroivanov/cfn-tools",
+
+    author="Borislav Ivanov",
+    author_email="borogl@gmail.com",
+
+    description="Tools for AWS CloudFormation",
+    long_description=open('README.rst').read(),
+
+    packages=[
+        'cfntools',
+    ],
+    package_dir={'cfntools':
+                 'cfntools'},
+    entry_points={
+        'console_scripts': [
+            'cfn-tools=cfntools.cli:main'
+        ]
+    },
+
+    install_requires=requirements,
+    license="MIT license",
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
